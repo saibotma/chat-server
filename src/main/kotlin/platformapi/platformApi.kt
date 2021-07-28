18fe2de7407c2197851ffbbc1ec50548c7e9bf2d
Tobias Marschall <tobias.marschall@online.de>
2021-07-28 11:30:18 +0200

    add platform api structure

--- src/main/kotlin/platformapi/platformApi.kt
+++ src/main/kotlin/platformapi/platformApi.kt
@@ -1,6 +1,31 @@
 package platformapi
 
+import io.ktor.locations.*
 import io.ktor.routing.*
+import java.util.*
+
+@Location("/channels")
+object ChannelList {
+    @Location("/{channelId}")
+    data class ChannelDetails(val channelId: UUID, val channelList: ChannelList) {
+        @Location("/members")
+        data class ChannelMemberList(val channelDetails: ChannelDetails) {
+            @Location("/{userId}")
+            data class ChannelMemberDetails(val userId: String, val channelMembers: ChannelMemberList) {
+
+            }
+        }
+    }
+}
+
+@Location("/users")
+object UserList {
+    @Location("/{userId}")
+    data class UserDetails(val userId: String, val userList: UserList) {
+        @Location("/tokens")
+        data class UserTokenList(val userDetails: UserDetails)
+    }
+}
 
 fun Route.installPlatformApi() {
 
