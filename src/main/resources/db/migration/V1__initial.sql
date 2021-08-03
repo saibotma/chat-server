1309e2643a0367a7428d39a5d5da6e7d6275554c
Tobias Marschall <tobias.marschall@online.de>
2021-08-03 08:09:35 +0200

    add quotation marks

--- src/main/resources/db/migration/V1__initial.sql
+++ src/main/resources/db/migration/V1__initial.sql
@@ -6,7 +6,7 @@ CREATE TABLE "user"
     CONSTRAINT "user_id_pkey" PRIMARY KEY ("id")
 );
 
-CREATE TABLE channel
+CREATE TABLE "channel"
 (
     "id"         uuid,
     "name"       varchar(128) NULL,
@@ -15,16 +15,16 @@ CREATE TABLE channel
     CONSTRAINT "channel_id_pkey" PRIMARY KEY ("id")
 );
 
-CREATE TABLE channel_member
+CREATE TABLE "channel_member"
 (
-    channel_id uuid,
-    "user_id"  varchar,
-    "role"     channel_member_role NOT NULL,
-    CONSTRAINT channel_member_channel_id_user_id_pkey
-        PRIMARY KEY (channel_id, "user_id"),
-    CONSTRAINT channel_member_channel_id_fkey
-        FOREIGN KEY (channel_id) REFERENCES channel ("id"),
-    CONSTRAINT channel_member_user_id_fkey
+    "channel_id" uuid,
+    "user_id"    varchar,
+    "role"       channel_member_role NOT NULL,
+    CONSTRAINT "channel_member_channel_id_user_id_pkey"
+        PRIMARY KEY ("channel_id", "user_id"),
+    CONSTRAINT "channel_member_channel_id_fkey"
+        FOREIGN KEY ("channel_id") REFERENCES "channel" ("id"),
+    CONSTRAINT "channel_member_user_id_fkey"
         FOREIGN KEY ("user_id") REFERENCES "user" ("id")
 );
 
@@ -34,8 +34,8 @@ CREATE TABLE "chat_message"
     "text"                 varchar(1048) NOT NULL,
     "responded_message_id" uuid,
     "extended_message_id"  uuid,
-    creator_user_id        varchar,
-    channel_id             uuid          NOT NULL,
+    "creator_user_id"      varchar,
+    "channel_id"           uuid          NOT NULL,
     "created_at"           timestamptz   NOT NULL,
     CONSTRAINT "chat_message_id_pkey" PRIMARY KEY ("id"),
     CONSTRAINT "chat_message_responded_message_id_fkey"
@@ -43,7 +43,7 @@ CREATE TABLE "chat_message"
     CONSTRAINT "chat_message_extended_message_id_fkey"
         FOREIGN KEY ("extended_message_id") REFERENCES "chat_message" ("id") ON DELETE SET NULL,
     CONSTRAINT "chat_message_creator_user_id_fkey"
-        FOREIGN KEY (creator_user_id) REFERENCES "user" ("id") ON DELETE SET NULL,
-    CONSTRAINT chat_message_channel_id_fkey
-        FOREIGN KEY (channel_id) REFERENCES channel ("id") ON DELETE CASCADE
+        FOREIGN KEY ("creator_user_id") REFERENCES "user" ("id") ON DELETE SET NULL,
+    CONSTRAINT "chat_message_channel_id_fkey"
+        FOREIGN KEY ("channel_id") REFERENCES "channel" ("id") ON DELETE CASCADE
 );
