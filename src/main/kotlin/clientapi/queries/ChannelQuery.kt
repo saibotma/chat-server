18cf831b7c7fb645699c8b16aab2856ca0d85f03
Tobias Marschall <tobias.marschall@online.de>
2021-08-04 10:04:44 +0200

    implement channel list

--- src/main/kotlin/clientapi/queries/ChannelQuery.kt
+++ src/main/kotlin/clientapi/queries/ChannelQuery.kt
@@ -3,11 +3,14 @@ package clientapi.queries
 import clientapi.AuthContext
 import clientapi.models.DetailedChannel
 import persistence.jooq.KotlinDslContext
+import persistence.postgres.queries.getChannelsOf
 
 class ChannelQuery(
     private val database: KotlinDslContext,
 ) {
-    fun channels(context: AuthContext): List<DetailedChannel> {
-        TODO()
+    suspend fun channels(context: AuthContext): List<DetailedChannel> {
+        return database.transaction {
+            getChannelsOf(context.userId)
+        }
     }
 }
