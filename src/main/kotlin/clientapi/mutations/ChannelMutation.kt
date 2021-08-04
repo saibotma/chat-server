6cda503518c57eb1f8757b2ee38bfeec76592f5b
Tobias Marschall <tobias.marschall@online.de>
2021-08-04 11:33:27 +0200

    fix typo

--- src/main/kotlin/clientapi/mutations/ChannelMutation.kt
+++ src/main/kotlin/clientapi/mutations/ChannelMutation.kt
@@ -8,7 +8,6 @@ import persistence.postgres.queries.getChannelsOf
 import persistence.postgres.queries.insertChannel
 import persistence.postgres.queries.insertMembers
 import java.time.Instant.now
-import java.util.*
 import java.util.UUID.randomUUID
 
 class ChannelMutation(
@@ -17,12 +16,12 @@ class ChannelMutation(
     suspend fun addChannel(
         context: AuthContext,
         name: String?,
-        memberIds: List<String>
+        memberUserIds: List<String>
     ): clientapi.models.DetailedChannel {
         return database.transaction {
             val id = randomUUID()
-            insertChannel(Channel(id = randomUUID(), name = name, isManaged = false, createdAt = now()))
-            insertMembers(memberIds.map { ChannelMember(channelId = id, userId = it) })
+            insertChannel(Channel(id = id, name = name, isManaged = false, createdAt = now()))
+            insertMembers(memberUserIds.map { ChannelMember(channelId = id, userId = it) })
             getChannelsOf(userId = context.userId, channelIdFilter = id).first()
         }
     }
