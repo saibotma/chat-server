f9519b15aea2daf49c4922e7a5161e9c18f94007
Tobias Marschall <tobias.marschall@online.de>
2021-08-05 10:45:55 +0200

    handle permissions for message query

--- src/main/kotlin/clientapi/queries/MessageQuery.kt
+++ src/main/kotlin/clientapi/queries/MessageQuery.kt
@@ -1,14 +1,19 @@
 package clientapi.queries
 
 import clientapi.AuthContext
+import clientapi.ClientApiException
 import clientapi.models.DetailedMessage
+import clientapi.resourceNotFound
 import persistence.jooq.KotlinDslContext
 import persistence.postgres.queries.getMessagesOf
+import persistence.postgres.queries.isMemberOfChannel
 import java.util.*
 
 class MessageQuery(private val database: KotlinDslContext) {
     suspend fun messages(context: AuthContext, channelId: UUID): List<DetailedMessage> {
+        val userId = context.userId
         return database.transaction {
+            if (!isMemberOfChannel(channelId = channelId, userId = userId)) throw ClientApiException.resourceNotFound()
             getMessagesOf(channelId = channelId, userId = context.userId)
         }
     }
