50a00029d546e1cd59b1b090bce9361a526f9b2e
Tobias Marschall <tobias.marschall@online.de>
2021-08-09 09:02:42 +0200

    test platform api authentication

---
+++ src/test/kotlin/platformapi/PlatformApiAccessTokenAuthentication.kt
@@ -0,0 +1,41 @@
+package platformapi
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.kotest.matchers.shouldBe
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import org.junit.jupiter.api.Test
+import org.kodein.di.instance
+import testutil.ServerTestEnvironment
+import testutil.mockedUser
+import testutil.serverTest
+
+class PlatformApiAccessTokenAuthentication {
+    @Test
+    fun `grants access with a valid token`() {
+        serverTest {
+            val platformApiConfig: PlatformApiConfig by di.instance()
+            val response = sendRequest(platformApiConfig.accessToken)
+            response.status() shouldBe HttpStatusCode.Created
+        }
+    }
+
+    @Test
+    fun `returns an error when the token is invalid`() {
+        serverTest {
+            val response = sendRequest("invalid token")
+            response.status() shouldBe HttpStatusCode.Unauthorized
+        }
+    }
+
+    private fun ServerTestEnvironment.sendRequest(token: String): TestApplicationResponse {
+        val user = mockedUser()
+        return testApplicationEngine.handleRequest(HttpMethod.Put, "/platform/users/${user.id}") {
+            val objectMapper = jacksonObjectMapper()
+            addHeader("Content-Type", "application/json")
+            addHeader("X-Chat-Server-Platform-Api-Access-Token", token)
+            val body = objectMapper.writeValueAsString(user)
+            setBody(body)
+        }.response
+    }
+}
