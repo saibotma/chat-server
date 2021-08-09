8a12df66b65f59547ca0c5e0b62942bed3307f8d
Tobias Marschall <tobias.marschall@online.de>
2021-08-09 08:51:11 +0200

    test client api authentication

---
+++ src/test/kotlin/clientapi/ClientApiJwtAuthenticationTests.kt
@@ -0,0 +1,59 @@
+package clientapi
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.kotest.matchers.shouldBe
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import org.junit.jupiter.api.Test
+import org.kodein.di.instance
+import testutil.ServerTestEnvironment
+import testutil.serverTest
+import java.time.Instant.now
+import java.util.*
+
+class ClientApiJwtAuthenticationTests {
+    @Test
+    fun `grants access with a valid jwt`() {
+        serverTest {
+            val (_, user) = createUser()
+            val response = sendRequest(createUserToken(userId = user!!.id).second!!.jwt)
+            response.status() shouldBe HttpStatusCode.OK
+        }
+    }
+
+    @Test
+    fun `returns an error when the token has an unknown subject`() {
+        serverTest {
+            val clientApiConfig: ClientApiConfig by di.instance()
+            val response = sendRequest(
+                JWT.create().withSubject("invalid subject").sign(Algorithm.HMAC256(clientApiConfig.jwtSecret))
+            )
+            response.status() shouldBe HttpStatusCode.Unauthorized
+        }
+    }
+
+    @Test
+    fun `returns an error when the token is expired`() {
+        serverTest {
+            val (_, user) = createUser()
+            val clientApiConfig: ClientApiConfig by di.instance()
+            val response = sendRequest(
+                JWT.create().withSubject(user!!.id).withExpiresAt(Date.from(now().minusSeconds(60)))
+                    .sign(Algorithm.HMAC256(clientApiConfig.jwtSecret))
+            )
+            response.status() shouldBe HttpStatusCode.Unauthorized
+        }
+    }
+
+    private fun ServerTestEnvironment.sendRequest(jwt: String): TestApplicationResponse {
+        return testApplicationEngine.handleRequest(HttpMethod.Post, "/client/graphql") {
+            val objectMapper = jacksonObjectMapper()
+            addHeader("Content-Type", "application/json")
+            addHeader("Authorization", "Bearer $jwt")
+            val body = objectMapper.writeValueAsString(mapOf("query" to "{ channels { id } }"))
+            setBody(body)
+        }.response
+    }
+}
