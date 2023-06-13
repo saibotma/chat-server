b5951bf1c7edcf65e874150e281be5b76c47a4ed
saibotma <tobias.marschall@online.de>
2023-06-13 08:10:56 +0200

    build docker image for linux/amd64

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 ./gradlew shadowJar
 version=$(./gradlew -q printVersion)
-docker build -t saibotma/chat-server:latest .
+docker build --platform linux/amd64 -t saibotma/chat-server:latest .
 docker push saibotma/chat-server:latest
 docker tag saibotma/chat-server:latest saibotma/chat-server:"$version"
 docker push saibotma/chat-server:"$version"
